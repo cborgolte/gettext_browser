98e6ea2fbc0368b6bd65563334464af538d44e05
Christoph Borgolte <christoph.borgolte@googlemail.com>
2012-05-23 22:34:30 +0200

    Added error templates.

--- gettext_browser/settings.py
+++ gettext_browser/settings.py
@@ -1,6 +1,8 @@
 # Django settings for gettext_browser project.
 
-DEBUG = True
+import os.path
+
+DEBUG = False
 TEMPLATE_DEBUG = DEBUG
 
 ADMINS = (
@@ -106,6 +108,7 @@ ROOT_URLCONF = 'gettext_browser.urls'
 WSGI_APPLICATION = 'gettext_browser.wsgi.application'
 
 TEMPLATE_DIRS = (
+    os.path.join(os.path.dirname(__file__), 'templates'),
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
